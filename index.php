535f219ee65721a96932f5eb519eb1c142c8aa1a
Kyle Vanderburg <kyle@noteforge.com>
2024-12-15 04:42:50 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="//cdn.ntfg.net/common/fonts.css" type="text/css" />
+	<link rel="stylesheet" href="//cdn.ntfg.net/common/nfcore.css">
+	<link rel="shortcut icon" href="//ndsucomposition.com/assets/favicon.png" />
+    <title>New Music North Dakota</title>
+    <style>
+		A {color:#0a5640 !IMPORTANT;}
+		A:not(.btn):not(.nav-link):not(.homename):not(.navbar-brand){text-decoration:underline;}
+		.btn-link {color:#0a5640;text-decoration: none;}
+		a:hover:not(.navbar-brand):not(.btn-ndsu-green):not(.homename):not() {color:#4a4a4a;}
+		A.btn{color:#fff;}
+		body{}
+    </style>
+</head>
+<body>
+<?php include '/var/www/ndsucomposition.com/htdocs/header.php'; ?>
+
+    <header>
+        <div class="container">
+        <hr>
+        <img src="assets/NMND Horizontal.png" alt="New Music ND" style="width:200px;" /> <span class="float-end"><h4>New Music Initiatives from NDSU Music Composition</h4></span>
+		</div>
+	</header>
+	
+	<div class="container">
+		<hr>
+	<h1>NDSU Initiatives</h1>
+		<table class="table table-striped">
+            <thead><th>No.</th><th>Title</th><th>Category</th><th>Status</th></thead>
+            <tbody>
+                <tr><td>NMND1</td><td>NDSU Composition Stickers</td><td>Recruitment/Promotion</td><td>Done</td></tr>
+                <tr><td>NMND2</td><td>Composition Cookbook</td><td>Recruitment/Promotion</td><td>Done</td></tr>
+                <tr><td>NMND3</td><td>Composer Pins</td><td>Culture</td><td>Done</td></tr>
+                <tr><td>NMND4</td><td>Studio Swag</td><td>Recruitment/Promotion</td><td>In Progress</td></tr>
+                <tr><td>NMND5</td><td>Thundering Heard New Music Series</td><td>Events</td><td>Done</td></tr>
+                <tr><td>NMND6</td><td>Composer's Forum</td><td>Culture</td><td>Progress</td></tr>
+                <tr><td>NMND7</td><td>Score Wars</td><td>Recruitment/Promotion</td><td>Done</td></tr>
+                <tr><td>NMND8</td><td>New Music Punchcards</td><td>Recruitment/Promotion</td><td>Progress</td></tr>
+                <tr><td>NMND9</td><td>NDSU Composition Anthology</td><td>Recruitment/Promotion/Culture</td><td>Progress</td></tr>
+                <tr><td>NMND10</td><td>Fargo Composers Press</td><td>Recruitment/Promotion/Culture</td><td>Progress</td></tr>
+                <tr><td>NMND11</td><td>Create Event Promotional Plan</td><td>Promotion/Culture</td><td>Progress</td></tr>
+                <tr><td>NMND12</td><td>Develop Area Music Director List</td><td>Promotion</td><td>Progress</td></tr>
+                <tr><td>NMND13</td><td>Create Composer Subcommitties for Promotion, Operations, etc.</td><td>Promotion</td><td>Progress</td></tr>
+            </tbody>
+        </table>
+		
+	</div>
+	<?php include '/var/www/ndsucomposition.com/htdocs/footer.php'; ?>
+</body>
+</html>
---
+++ nmnd-index.php
@@ -0,0 +1,112 @@
+<?php
+
+function composition($title,$composer,$image,$link,$note="")
+{
+	
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+		<title>New Music North Dakota</title>
+		<link rel="shortcut icon" href="//newmusicnd.org/assets/NMNDfavicon.png"/>
+		<!-- Font Awesome icons (free version)-->
+		<script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+		<!-- Google fonts-->
+		<link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+		<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+		<!-- Core theme CSS (includes Bootstrap)-->
+		<link href="css/styles.css" rel="stylesheet" />
+	</head>
+	<body id="page-top">
+		<!-- Navigation-->
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+			<a class="navbar-brand js-scroll-trigger" href="#page-top">
+				<span class="d-block d-lg-none">New Music North Dakota</span>
+				<span class="d-none d-lg-block"><img class="img-fluid mx-auto mb-2" src="NMND-web.png" alt="..." /></span>
+			</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<!--<ul class="navbar-nav">
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#solo">Solo</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#chamber">Chamber</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#choral">Choral</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#large-ensemble">Large Ensemble</a></li>
+				</ul>-->
+			</div>
+		</nav>
+		<!-- Page Content-->
+		<div class="container-fluid p-0">
+			<!-- About-->
+			<section class="resume-section" id="about">
+				<div class="resume-section-content">
+					<h1 class="mb-0">New Music North Dakota</h1>
+					<div class="subheading mb-5">
+						New Music Initiatives from NDSU Music Composition
+					</div>
+					<p class="lead mb-5">
+					<a href="//ndsucomposition.com">NDSU Music Composition</a><br />
+					<a href="//ndsucomposition.com/#resources">NDSU Music Composition Resources</a><br />
+					<a href="//ndsucomposition.com/#thundering-heard">Thundering Heard New Music Series</a><br />
+					<a href="//noisedots.com">NoiseDots Composer Collective</a><br />
+					</p>
+					<!--<div class="social-icons">
+						<a class="social-icon" href="#!"><i class="fab fa-linkedin-in"></i></a>
+						<a class="social-icon" href="#!"><i class="fab fa-github"></i></a>
+						<a class="social-icon" href="#!"><i class="fab fa-twitter"></i></a>
+						<a class="social-icon" href="#!"><i class="fab fa-facebook-f"></i></a>
+					</div>-->
+				</div>
+			</section>
+			<?php /*
+			<hr class="m-0" />
+			
+			<!-- Solo -->
+			<section class="resume-section" id="solo">
+				<div class="resume-section-content">
+					<h2 class="mb-5">Solo</h2>
+
+				</div>
+			</section>
+			<hr class="m-0" />
+			
+			<!-- Chamber-->
+			<section class="resume-section" id="chamber">
+				<div class="resume-section-content">
+					<h2 class="mb-5">Chamber Music</h2>
+					
+				</div>
+			</section>
+			<hr class="m-0" />
+			
+			<!-- Choral-->
+			<section class="resume-section" id="choral">
+				<div class="resume-section-content">
+					<h2 class="mb-5">Choral</h2>
+					
+				</div>
+			</section>
+			<hr class="m-0" />
+			
+			<!-- Large Ensemble-->
+			<section class="resume-section" id="large-ensemble">
+				<div class="resume-section-content">
+					<h2 class="mb-5">Large Ensemble</h2>
+
+				</div>
+			</section>
+			*/?>
+		</div>
+		<!-- Bootstrap core JS-->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+		<!-- Core theme JS-->
+		<script src="js/scripts.js"></script>
+	</body>
+</html>
